ed58460a99acad1df8884ac96f6f0a4bbd7d570c
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-26 10:19:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+const loginForm=document.getElementById('loginForm');
+const existingButton=document.getElementById('existing');
+
+	if(localStorage.getItem('username')&&localStorage.getItem('password')){
+		existingButton.style.display = 'block';
+	}
+	loginForm.addEventListener('submit',function(event)=>{
+		event.preventDefault();
+		const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const rememberMe = document.getElementById('checkbox').checked;
+
+        if (rememberMe) {
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+        } else {
+            localStorage.removeItem('username');
+            localStorage.removeItem('password');
+        }
+
+        alert(`Logged in as ${username}`);
+    });
+    existingButton.addEventListener('click', function() {
+        const username = localStorage.getItem('username');
+        alert(`Logged in as ${username}`);
+    });
+	
+});	
